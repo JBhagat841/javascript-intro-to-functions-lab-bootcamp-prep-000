e6d74c9b334fe66de279b4d86a01c6b324578a12
Jay Bhagat <jaybhagat841@gmail.com>
2018-07-12 19:43:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
   console.log(pib)
 }
 function sayHiToGrandma(string) {
-  var lowercase = 'string'
+  var lowercase = 'I can\'t hear you!'
  
 lowercase.toLowerCase() === lowercase // true
 }
