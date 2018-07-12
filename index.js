ef97a7d27306910a9c3ff0b9564ca3f072bd1811
Jay Bhagat <jaybhagat841@gmail.com>
2018-07-12 19:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,9 @@ function logWhisper(string) {
   console.log(pib)
 }
 function sayHiToGrandma(string) {
-  var hello = string.toLowerCase()
- 
-hello.toLowerCase() === hello // true
-console.log(hello)
-}
+  if (string === string.toLowerCase()) {
+     return "I can't hear you!"
+-  }else if (string === uppercase) {	+  }else if (string === string.toUpperCase()){
+     return "YES INDEED!"	     return "YES INDEED!"
+   }else if (string === "I love you, Grandma."){	   }else if (string === "I love you, Grandma."){
+     return "I love you, too."	     return "I love you, too." }
